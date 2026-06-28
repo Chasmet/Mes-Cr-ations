7383785031540063a33e0707067b020363b87724
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-06-28 10:52:36 +0200

    Update cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mes-creations-v1';
+const CACHE = 'mes-creations-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
@@ -10,5 +10,9 @@ self.addEventListener('activate', event => {
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
 });
